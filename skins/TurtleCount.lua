89eb46f215875da86de04844663c5cac31efd16e
Mr_Rosh <cosminrosca1995@gmail.com>
2023-05-17 19:46:28 +0300

    Add TurtleCount

---
+++ skins/TurtleCount.lua
@@ -0,0 +1,13 @@
+pfUI.addonskinner:RegisterSkin("TurtleCount", function()
+  local penv = pfUI:GetEnvironment()
+  local CreateBackdrop, CreateBackdropShadow = penv.CreateBackdrop, penv.CreateBackdropShadow
+
+  local tooltip_alpha = pfUI_config.tooltip.alpha
+  CreateBackdrop(TurtleCount,nil,nil,tonumber(pfUI_config.tooltip.alpha))
+  CreateBackdropShadow(TurtleCount)
+  TurtleCount:SetWidth(55)
+  TurtleCount:SetHeight(16)
+  TurtleCount.icon:SetPoint("LEFT", TurtleCount, "LEFT", 1, 0)
+  TurtleCount.text:SetFont(pfUI.font_default, pfUI_config.global.font_size, "OUTLINE")
+  pfUI.addonskinner:UnregisterSkin("TurtleCount")
+end)
