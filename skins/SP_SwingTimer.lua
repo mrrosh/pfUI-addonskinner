88902f3ed9708b84f17b4a7c8a5d68fc47a24781
Mr_Rosh <cosminrosca1995@gmail.com>
2025-02-22 00:16:05 +0200

    Update SP_SwingTimer.lua

--- skins/SP_SwingTimer.lua
+++ skins/SP_SwingTimer.lua
@@ -3,6 +3,8 @@ pfUI.addonskinner:RegisterSkin("SP_SwingTimer", function()
   if pfUI and pfUI.api and pfUI.api.CreateBackdrop then
     if SP_ST_Frame then
       pfUI.api.CreateBackdrop(SP_ST_Frame)
+      pfUI.api.CreateBackdrop(SP_ST_FrameOFF)
+      pfUI.api.CreateBackdrop(SP_ST_FrameRange)
       local font_string = getglobal("SP_ST_FrameText")
       if font_string and font_string.SetFont then
         font_string:SetFont(pfUI_config.global.font_combat, 14, "NORMAL")
